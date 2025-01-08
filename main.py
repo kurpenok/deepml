ee8bab72902b1a62c8c63c3fac5c94b2154448d1
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-01-08 16:55:59 +0600

    Added solution of 4 problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tasks.t0001_matrix_times_vector import matrix_dot_vector
 from tasks.t0002_transpose_of_a_matrix import transpose_matrix
 from tasks.t0003_reshape_matrix import reshape_matrix
+from tasks.t0004_calculate_mean_by_row_or_column import calculate_matrix_mean
 
 
 # 0001. Matrix times Vector
@@ -19,3 +20,15 @@ assert reshape_matrix([[1, 2, 3, 4], [5, 6, 7, 8]], (4, 2)) == [
     [7, 8],
 ]
 assert reshape_matrix([[1, 2, 3], [4, 5, 6]], (3, 2)) == [[1, 2], [3, 4], [5, 6]]
+
+# 0004. Calculate Mean by Row or Column
+assert calculate_matrix_mean([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "column") == [
+    4.0,
+    5.0,
+    6.0,
+]
+assert calculate_matrix_mean([[1, 2, 3], [4, 5, 6], [7, 8, 9]], "row") == [
+    2.0,
+    5.0,
+    8.0,
+]
