a69a150f6b7fb9e44b8c4fd84f16e8bcb64025aa
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-03-17 19:13:53 +0600

    Added solution of problem 48

---
+++ solutions/s0048_implement_reduced_row_echelon_form_function.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def rref(matrix: np.ndarray) -> np.ndarray:
+    A = matrix.astype(np.float32)
+    n, _ = A.shape
+
+    for i in range(n):
+        if A[i, i] == 0:
+            nonzero_rel_id = np.nonzero(A[i:, i])[0]
+            if len(nonzero_rel_id) == 0:
+                continue
+
+            A[i] = A[i] + A[nonzero_rel_id[0] + i]
+
+        A[i] = A[i] / A[i, i]
+        for j in range(n):
+            if i != j:
+                A[j] -= A[j, i] * A[i]
+
+    return A
