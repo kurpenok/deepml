487694b0e5adf0a848c2e62be2890f67e29193a4
Kuat Kurpenov <kurpenov2003@yandex.ru>
2026-02-20 22:55:09 +0300

    [+] Added solution of problem 1

---
+++ src/s0001_matrix_vector_dot_product.py
@@ -0,0 +1,6 @@
+def matrix_dot_vector(
+    a: list[list[int | float]], b: list[int | float]
+) -> int | list[int | float]:
+    if len(a[0]) != len(b):
+        return -1
+    return [sum(x * y for x, y in zip(row, b)) for row in a]
